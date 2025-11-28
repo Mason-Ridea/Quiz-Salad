62c57397fd9e88b4337a8c483300c0e27128f16f
mason <masonrideabuisness@gmail.com>
2025-11-27 20:40:04 -0500

    added randomness

--- scripts/Background.js
+++ scripts/Background.js
@@ -16,12 +16,14 @@ class triangle {
         const buffer = new THREE.BufferGeometry();
         buffer.setAttribute('position', new THREE.BufferAttribute(vertices,3)) 
         const material = new THREE.MeshBasicMaterial({color : 0xff0000, side :THREE.DoubleSide }) 
+        
         //declares the class varibles
         this.triangle = new THREE.Mesh(buffer, material);
         this.triangle.position.x = initalXPos;
         this.triangle.position.y = initalYPos;
         this.Xspeed = initalXSpeed;
         this.Yspeed = initalYSpeed;
+        console.log(`${initalXSpeed} ${initalYSpeed}`)
         this.rotationSpeed = initalRotationSpeed 
     }
     step(){
@@ -32,14 +34,12 @@ class triangle {
         //sets triangle to the opposing side of the screen if it goes out of bounds
         this.triangle.position.x = (this.triangle.position.x > 7.25) ? -7.25 : this.triangle.position.x;
         this.triangle.position.x = (this.triangle.position.x < -7.25) ? 7.25 : this.triangle.position.x;
-        this.triangle.position.y = (this.triangle.position.y  > 6) ? -4 : this.triangle.position.y;
-        this.triangle.position.y = (this.triangle.position.y < -4) ? 6 : this.triangle.position.y;
-        console.log(this.triangle.position.y);
+        this.triangle.position.y = (this.triangle.position.y  > 5.05) ? -4 : this.triangle.position.y;
+        this.triangle.position.y = (this.triangle.position.y < -4) ? 5.45 : this.triangle.position.y;
     }
 }
 //creates the esstentials
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xFFFFFF);
 const camera = new THREE.PerspectiveCamera(75, WindowWidth/WindowHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -53,7 +53,30 @@ function animate(){
     renderer.render(scene, camera);
 }
 function addTriangle(){
-    let newTriangle = new triangle(-4,2,0.0,0.03,0.01)
+    let newTriangle = new triangle(-4,2,RandomRange(-0.05,0.05),RandomRange(-0.002,0.002),0.01)
     scene.add(newTriangle.triangle)
     return newTriangle
+}
+function RandomRange(min, max){
+    // swaps min and max is min was actually more than max
+    if (min > max){
+        let tempMin = min;
+        min = max;
+        max = tempMin
+    }
+    //checks if one is negative
+    if (min < 0){
+        //50% falls in the negatives and percent of the time is in the positives 
+        if(Math.random() > 0.5){
+            return Math.random() * max
+        }
+        else{
+            return Math.random() * min
+        }
+    }
+    else{
+        //returns a regular range else
+        return Math.random() * (max - min) + min;
+    }
+
 }
